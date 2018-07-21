9ce1640ef6f0ef1e77dc547061ec78817055ef10
AriesLL <memoryzpp@gmail.com>
2018-07-20 18:46:39 -0700

    deleted:    test.sh

--- gatk4/test.sh
+++
@@ -1,6 +0,0 @@
-bamDir="/genome/ssd2/peipei/0inputUnmappedBAM"
-refDir="/genome/ssd2/peipei/1reference"
-input_bam="${bamDir}/NA12878_falcon.query.sorted.unmapped.bam"
-unmapped_bam_base=`basename $input_bam .unmapped.bam` 
-echo $unmapped_bam_base
-
